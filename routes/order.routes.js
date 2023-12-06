86cf779c38d788350ce27cc1aecddfead4c77102
MeenaThirunaukkarasu <meena.thirunau@gmail.com>
2023-12-06 19:00:43 +0100

    setting up mailjet

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
 const Order = require("../models/Order.model");
-const Cart=require('../models/Cart.model')
-const Product=require('../models/Product.model')
+const Cart = require("../models/Cart.model");
+const Product = require("../models/Product.model");
 const { Client } = require("square");
 
 const squareClient = new Client({
@@ -12,67 +12,61 @@ const squareClient = new Client({
     "EAAAENb-aRCfGlemDbi_tS4UTAmcCDap14BovcFw4t4wJbp86RnT_FKoGhuMxd8m",
 });
 
+const mailjet = require("node-mailjet").apiConnect(
+  "c3ec2f57947960e5cd3464735cbc6ac1",
+  "bf06d993a022b723f04df1866fd02d52"
+);
+const sendGeneralMail = function (mail, sub, msg) {
+  return mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Name: "ST Hospital",
+          Email: "sunithajosephine22@gmail.com",
+        },
+        To: [
+          {
+            Email: "sunithatheresa18@gmail.com",
+            Name: "Sunitha",
+          },
+        ],
+        Subject: sub,
+        TextPart: msg,
+      },
+    ],
+  });
+};
+
 router.get("/", (req, res, next) => {
   Order.find()
     .populate("user")
-    .populate('cartDetails.product')
+    .populate("cartDetails.product")
     .then((order) => {
       res.json(order);
     });
 });
 
 router.get("/:id", (req, res, next) => {
-const userId=req.params.id
-  Order.find({user:userId})
+  const userId = req.params.id;
+  Order.find({ user: userId })
     .populate("user")
-    .populate('cartDetails.product')
+    .populate("cartDetails.product")
     .then((order) => {
       res.json(order);
     });
-}); 
+});
 
 router.patch("/update/:id", (req, res, next) => {
-const orderId=req.params.id
-  Order.findByIdAndUpdate(orderId,{ status: 'completed' }, { new: true })
+  const orderId = req.params.id;
+  Order.findByIdAndUpdate(orderId, { status: "completed" }, { new: true })
     .then((order) => {
       res.json(order);
     })
-    .catch(error=>{
-      res.json('error updating status');
-
-    })
+    .catch((error) => {
+      res.json("error updating status");
+    });
 });
-// router.post("/create", async (req, res, next) => {
-//   const { cart, user, overallTotal, deliveryAddress } = req.body;
-//   console.log(deliveryAddress);
-//   console.log("cart", cart.cartDetails);
-//   Order.create({
-//     user: user._id,
-//     cartDetails: cart.cartDetails,
-//     totalAmount: overallTotal,
-//     deliveryAddress: deliveryAddress._id,
-//   })
-//     .then((createdOrder) => {
-//       createdOrder.cartDetails.forEach(cart=>{
-// const cartId=cart.product
-//       return  Product.findByIdAndUpdate({cartId},{quantity:quantity-cart.quantity},{new:true})
-//       })
-//       .then(updatedProduct=>{
-//         Cart.findByIdAndUpdate(cart._id,
-//           { $set: { cartDetails: [] } }, 
-//           { new: true })
-//           .then(updatedCart=>{
-//             res.json(createdOrder);
-        
-//           })
-//       })
 
-//     })
-//     .catch((error) => {
-//       console.log("error", error);
-//     });
-  
-// });
 router.post("/create", (req, res, next) => {
   const { cart, user, overallTotal, deliveryAddress } = req.body;
 
@@ -82,33 +76,39 @@ router.post("/create", (req, res, next) => {
     totalAmount: overallTotal,
     deliveryAddress: deliveryAddress._id,
   })
+    .populate("user")
     .then((createdOrder) => {
-      console.log("Cart Details Length:", createdOrder.cartDetails.length);
-console.log("Cart Details:", createdOrder.cartDetails);
-const updateProductPromises = createdOrder.cartDetails.map((cartItem) => {
-  const cartId = cartItem.product;
-  const newQuantity = parseInt(cartItem.quantity, 10);
+      sendGeneralMail(
+        `${createdOrder.user.email}`,
+        "Order Confirmation",
+        `Hi ${createdOrder.user.name} you have placed an order on SAM Shoppy for ${createdOrder.totalAmount}.For Further details about your order visit the  website https://flourishing-halva-5e3584.netlify.app/ `
+      );
+      const updateProductPromises = createdOrder.cartDetails.map((cartItem) => {
+        const cartId = cartItem.product;
+        const newQuantity = parseInt(cartItem.quantity, 10);
 
-  // Assume 'availability' is a string field in your Product model
-  return Product.findById(cartId).then((product) => {
-    if (!product) {
-      // Handle the case where the product is not found
-      throw new Error("Product not found");
-    }
+        // Assume 'availability' is a string field in your Product model
+        return Product.findById(cartId).then((product) => {
+          if (!product) {
+            // Handle the case where the product is not found
+            throw new Error("Product not found");
+          }
 
-    const currentAvailability = parseInt(product.availability, 10);
+          const currentAvailability = parseInt(product.availability, 10);
 
-    // Calculate the new availability value
-    const newAvailability = (currentAvailability - newQuantity).toString();
+          // Calculate the new availability value
+          const newAvailability = (
+            currentAvailability - newQuantity
+          ).toString();
 
-    // Update the product with the new availability value
-    return Product.findByIdAndUpdate(
-      cartId,
-      { $set: { availability: newAvailability } }, // Set new availability
-      { new: true }
-    );
+          // Update the product with the new availability value
+          return Product.findByIdAndUpdate(
+            cartId,
+            { $set: { availability: newAvailability } }, // Set new availability
+            { new: true }
+          );
+        });
       });
-    });
 
       // Wait for all product updates to complete
       return Promise.all(updateProductPromises)
@@ -131,6 +131,5 @@ const updateProductPromises = createdOrder.cartDetails.map((cartItem) => {
       res.status(500).json({ error: "Internal Server Error" });
     });
 });
-
 
 module.exports = router;
